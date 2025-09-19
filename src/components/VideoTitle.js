ef95abd8b3bd780395cb19ca305c652f4a189075
OmkarSantoshChavan <omkarschavan4@gmail.com>
2025-09-19 11:49:30 +0530

    TMDB api integration started

---
+++ src/components/VideoTitle.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const VideoTitle = (props) => {
+    const { title, overview } = props;
+  return (
+    <div className='pt-80 w-1/4 px-10 absolute text-white w-screen aspect-video bg-gradient-to-r from-black'>
+      <h1 className='text-4xl font-bold'>{title}</h1>
+      <p>{overview}</p>
+      <div>
+        <button className='m-2 px-4 py-1 bg-white rounded-md text-black hover:bg-opacity-80'>▶️ Play</button>
+        <button className='m-2 px-4 py-1 bg-gray-600 rounded-md text-white'>ℹ️ More Info</button>
+      </div>
+    </div>
+  )
+}
+
+export default VideoTitle
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,13 @@
 export const LOGO = "https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production_2025-08-26/consent/87b6a5c0-0104-4e96-a291-092c11350111/0198e689-25fa-7d64-bb49-0f7e75f898d2/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
 export const BODY_BG = "https://assets.nflxext.com/ffe/siteui/vlv3/0b0dad79-ad4d-42b7-b779-8518da389976/web/IN-en-20250908-TRIFECTA-perspective_0647b106-80e1-4d25-9649-63099752b49a_large.jpg"
+
+export const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2NWM2NDg0NGUzODZhMTlmOTY3NzQzZTQ4N2Q4MTMyMSIsIm5iZiI6MTc1ODIwMTkwOS43NTcsInN1YiI6IjY4Y2MwODM1MWI2NTZmYjFjNzA3MzllMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.CmTkyYONuvVfxTzk579QkutZr8tTsr9oOjuE2y9n3fQ'
+  }
+};
+
+
+
